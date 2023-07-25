7b8768de77bf1a20f3f38b2b17501fd7106f88f6
lozaim <65390783+lozaim@users.noreply.github.com>
2023-07-25 19:14:17 +0300

    Create test-http-response.py

---
+++ test-http-response.py
@@ -0,0 +1,51 @@
+#
+# Source from https://github.com/prabhath 
+#
+# https://gist.github.com/prabhath/b15484538aec506c096e6c4facc6f4fd
+#
+import urllib3
+import boto3
+sns= boto3.client('sns')
+http = urllib3.PoolManager()
+
+urls = ['https://oauth2-qa.vpc.getqardio.com/status',
+        'https://qa-us-ecommerce.vpc.getqardio.com/status',
+        'https://api-eu.getqardio.com/status',
+        'https://google.com'
+]
+topic_arn='arn:aws:sns:us-east-1:041027301676:DevOps-Email'
+
+def lambda_handler(event, context):
+    do_health_check()
+
+def do_health_check():
+    failures = list()
+    for url in urls:
+        try:
+            response = http.request('GET', url)
+            if response.status != 200:
+                error = dict()
+                temp=url.split('://')
+                error['url'] = '('+temp[0]+') '+temp[1]
+                error['status'] = response.status
+                error['response'] = response.data
+                failures.append(error)
+        except Exception as e:
+            error = dict()
+            temp=url.split('://')
+            error['url'] = '('+temp[0]+') '+temp[1]
+            error['status'] = 'xxx'
+            error['response'] = str(e)
+            failures.append(error)
+            message = "Error occurred for url: " + error['url']+ ". Error: " + str(e)
+            print(message)
+            
+    if len(failures) > 0:
+        message = "Health check failures in following. \n\n"
+        for failure in failures:
+            message += repr(failure) + '\n'
+        print('Sending message:'+ message)
+        # Publish to topic
+        sns.publish(TopicArn=topic_arn,
+                    Message=message,
+                    Subject="Health check failures in cloud component(s)")
